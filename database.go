ba7e8368da4a3f3dbe24248b7b2d1963b630a71a
VladislavEF <vefimov@progmaticlab.com>
2024-06-08 12:56:53 +0300

    Add database stucture and interface

---
+++ database.go
@@ -0,0 +1,49 @@
+package main
+
+type MemoryStorage struct {
+	// UserName on UserId
+	userIds map[string]string
+	// UserId on TaskIds
+	taskList map[string][]string
+	// TaskId on TaskInfo
+	tasks map[string]TaskInfo
+}
+
+type TaskInfo struct {
+	name      string
+	status    TaskStatus
+	dependens []string
+}
+
+type TaskStatus string
+
+type Database interface {
+	GetUserId(string) string
+	GetUserTasks(string) []string
+	GetTaskInfo(string) TaskInfo
+
+	AddUser(string)
+	AddUserTask(string)
+	AddNewTask(TaskInfo)
+}
+
+func GetDatabase() *Database {
+	return new(Database)
+}
+
+func (this *MemoryStorage) GetUserId(string) string {
+	return ""
+}
+func (this *MemoryStorage) GetUserTasks(string) []string {
+	return []string{""}
+}
+func (this *MemoryStorage) GetTaskInfo(string) TaskInfo {
+	return TaskInfo{}
+}
+
+func (this *MemoryStorage) AddUser(string) {
+}
+func (this *MemoryStorage) AddUserTask(string) {
+}
+func (this *MemoryStorage) AddNewTask(TaskInfo) {
+}
