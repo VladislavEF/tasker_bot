9c710e6718dfcfc7bb22e4e80f9fd06a46f83ca1
VladislavEF <vefimov@progmaticlab.com>
2024-06-08 21:23:51 +0300

    added code to open and save local db

--- database.go
+++ database.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type MemoryStorage struct {
 	// UserName on UserId
 	userIds map[string]string
@@ -15,27 +20,62 @@ type Database interface {
 	GetTaskInfo(string) TaskInfo
 
 	AddUser(string)
-	AddUserTask(string)
+	AddUserTask(string, string)
 	AddNewTask(TaskInfo)
+
+	Save() error
 }
 
 func GetDatabase() *Database {
 	return new(Database)
 }
 
-func (this *MemoryStorage) GetUserId(string) string {
+func (this *MemoryStorage) GetUserId(id string) string {
 	return ""
 }
-func (this *MemoryStorage) GetUserTasks(string) []string {
+func (this *MemoryStorage) GetUserTasks(id string) []string {
 	return []string{""}
 }
-func (this *MemoryStorage) GetTaskInfo(string) TaskInfo {
+func (this *MemoryStorage) GetTaskInfo(id string) TaskInfo {
 	return TaskInfo{}
 }
 
-func (this *MemoryStorage) AddUser(string) {
+func (this *MemoryStorage) AddUser(name string) {
+	if this.userIds == nil {
+		this.userIds = make(map[string]string)
+	}
 }
-func (this *MemoryStorage) AddUserTask(string) {
+func (this *MemoryStorage) AddUserTask(userId, taskId string) {
+	if this.taskList == nil {
+		this.taskList = make(map[string][]string)
+	}
 }
-func (this *MemoryStorage) AddNewTask(TaskInfo) {
+func (this *MemoryStorage) AddNewTask(task TaskInfo) {
+	if this.tasks == nil {
+		this.tasks = make(map[string]TaskInfo)
+	}
+}
+
+func (this *MemoryStorage) Save() error {
+	data, err := json.MarshalIndent(this, "", "	")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile("database.json", data, 0644)
+}
+
+func GetLocalDatabase() (Database, error) {
+	path := "database.json"
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	rawFile := []byte{}
+	_, err = file.Read(rawFile)
+	if err != nil {
+		return nil, err
+	}
+	db := &MemoryStorage{}
+	err = json.Unmarshal(rawFile, db)
+	return Database(db), err
 }
